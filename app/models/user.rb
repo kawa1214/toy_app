20203cfa278f1cad37d09c3b434289ae71a73d9b
Ryo Kawamura <kawa1214mura@gmail.com>
2022-06-19 21:38:53 +0900

    バリデーションを追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,6 @@
 class User < ApplicationRecord
     has_many :microposts
+    validates :name, length: { maximum: 20 }, presence: true
+    validates :email, length: { maximum: 40 }, presence: true
+
 end
